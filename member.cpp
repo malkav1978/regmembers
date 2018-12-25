7c97eb91aed51fa08069ed1af124767cbb11d315
Yannick Recht <yannick.recht@gmail.com>
2018-12-25 17:23:21 +0100

    Adding all the fields related to a single member registration form.

--- member.cpp
+++ member.cpp
@@ -1,4 +1,5 @@
-#include <QStringList>
+#include <QDate>
+#include <QString>
 
 #include "member.h"
 
@@ -13,62 +14,126 @@ Member::Member()
 //! Copy constructor, create a clone of another Member object.
 //!
 Member::Member(const Member &other)
-    : m_strName(other.name())
-    , m_strFirstname(other.firstname())
-    , m_strEmail(other.email())
+    : m_Name(other.name())
+    , m_Firstname(other.firstname())
+    , m_Email(other.email())
+    , m_PhoneNumber(other.phoneNumber())
+    , m_Alias(other.alias())
+    , m_Birthdate(other.birthdate())
+    , m_Birthplace(other.birthplace())
+    , m_Address(other.address())
+    , m_Postcode(other.postcode())
+    , m_City(other.city())
+    , m_MembershipDate(other.membershipDate())
 {
 }
 
-//!
-//! \brief Member::name
-//! \return
-//!
 QString Member::name() const
 {
-    return m_strName;
+    return m_Name;
 }
 
-//!
-//! \brief Member::setName
-//! \param name
-//!
 void Member::setName(const QString &name)
 {
-    m_strName = name;
+    m_Name = name;
 }
 
-//!
-//! \brief Member::firstname
-//! \return
-//!
 QString Member::firstname() const
 {
-    return m_strFirstname;
+    return m_Firstname;
 }
 
-//!
-//! \brief Member::setFirstname
-//! \param firstname
-//!
 void Member::setFirstname(const QString &firstname)
 {
-    m_strFirstname = firstname;
+    m_Firstname = firstname;
 }
 
-//!
-//! \brief Member::email
-//! \return
-//!
 QString Member::email() const
 {
-    return m_strEmail;
+    return m_Email;
 }
 
-//!
-//! \brief Member::setEmail
-//! \param email
-//!
 void Member::setEmail(const QString &email)
 {
-    m_strEmail = email;
+    m_Email = email;
+}
+
+QString Member::phoneNumber() const
+{
+    return m_PhoneNumber;
+}
+
+void Member::setPhoneNumber(const QString &phoneNumber)
+{
+    m_PhoneNumber = phoneNumber;
+}
+
+QString Member::alias() const
+{
+    return m_Alias;
+}
+
+void Member::setAlias(const QString &alias)
+{
+    m_Alias = alias;
+}
+
+QDate Member::birthdate() const
+{
+    return m_Birthdate;
+}
+
+void Member::setBirthdate(const QDate &birthdate)
+{
+    m_Birthdate = birthdate;
+}
+
+QString Member::birthplace() const
+{
+    return m_Birthplace;
+}
+
+void Member::setBirthplace(const QString &birthplace)
+{
+    m_Birthplace = birthplace;
+}
+
+QString Member::address() const
+{
+    return m_Address;
+}
+
+void Member::setAddress(const QString &address)
+{
+    m_Address = address;
+}
+
+QString Member::postcode() const
+{
+    return m_Postcode;
+}
+
+void Member::setPostcode(const QString &postcode)
+{
+    m_Postcode = postcode;
+}
+
+QString Member::city() const
+{
+    return m_City;
+}
+
+void Member::setCity(const QString &city)
+{
+    m_City = city;
+}
+
+QDate Member::membershipDate() const
+{
+    return m_MembershipDate;
+}
+
+void Member::setMembershipDate(const QDate &membershipDate)
+{
+    m_MembershipDate = membershipDate;
 }
--- member.h
+++ member.h
@@ -1,7 +1,8 @@
 #ifndef MEMBER_H
 #define MEMBER_H
 
-#include <QString>
+class QDate;
+class QString;
 
 //!
 //! \brief The Member class stores the information related to a single member.
@@ -23,15 +24,63 @@ public:
     QString email() const;
     void setEmail(const QString &email);
 
+    QString phoneNumber() const;
+    void setPhoneNumber(const QString &phoneNumber);
+
+    QString alias() const;
+    void setAlias(const QString &alias);
+
+    QDate birthdate() const;
+    void setBirthdate(const QDate &birthdate);
+
+    QString birthplace() const;
+    void setBirthplace(const QString &birthplace);
+
+    QString address() const;
+    void setAddress(const QString &address);
+
+    QString postcode() const;
+    void setPostcode(const QString &postcode);
+
+    QString city() const;
+    void setCity(const QString &city);
+
+    QDate membershipDate() const;
+    void setMembershipDate(const QDate &membershipDate);
+
 private:
     //! Holds the member's name.
-    QString m_strName;
+    QString m_Name;
 
     //! Holds the member's first name
-    QString m_strFirstname;
+    QString m_Firstname;
 
     //! Holds the member's e-mail address
-    QString m_strEmail;
+    QString m_Email;
+
+    //! Holds the member's phone number.
+    QString m_PhoneNumber;
+
+    //! Holds the member's nickname.
+    QString m_Alias;
+
+    //! Holds the member's birtdate.
+    QDate m_Birthdate;
+
+    //! Holds the member's birth place.
+    QString m_Birthplace;
+
+    //! Holds the member's address.
+    QString m_Address;
+
+    //! Holds the member's post code.
+    QString m_Postcode;
+
+    //! Holds the member's home city.
+    QString m_City;
+
+    //! Holds the member's last registration date.
+    QDate m_MembershipDate;
 
 };
 
